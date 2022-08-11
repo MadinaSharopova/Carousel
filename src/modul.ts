bb442182cd17049de28e6ccb4fe6d65a317ae53b
Madina <madina3089727@gmail.com>
2022-08-11 13:20:02 +0500

    Carousel

---
+++ src/modul.ts
@@ -0,0 +1,15 @@
+
+export interface CarouselType {
+    id: number
+    img: string
+    title: string
+}
+export interface Qiuz {
+    question: string
+    answerList: []
+
+}
+export interface QiuzanswerList{
+    answer:string
+    isCorrect:boolean
+}
